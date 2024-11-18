5a36315ea9b2aa6bc15d15073041b92928c42561
Delina01 <side21bo@student.ju.se>
2024-11-18 16:55:13 +0100

    leave game knappen

--- app/src/main/java/com/example/battleship_delin_hiba/Gamefunctions.kt
+++ app/src/main/java/com/example/battleship_delin_hiba/Gamefunctions.kt
@@ -1,8 +1,33 @@
 package com.example.battleship_delin_hiba
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BattleScreen(navController: NavController){
     /*
@@ -12,4 +37,43 @@ fun BattleScreen(navController: NavController){
     * value will be multiplied by 2 and if the result is 2 then it hit ship if it is zero then it missed
     * missed will have x on them hit will have a red dot on them
     * */
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton(
+                            onClick = {
+                                navController.popBackStack()
+                            }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                contentDescription = "back"
+                            )
+                        }
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { navController.navigate("Lobby") },
+                modifier = Modifier.padding(16.dp),
+                shape = CircleShape,
+                containerColor = Color(0xFFD3368E),
+                contentColor = Color.Black,
+                content = { Text("Leave Game") }
+            ) },
+        content = TODO()
+
+    )
+
 }
+
+
+
+
