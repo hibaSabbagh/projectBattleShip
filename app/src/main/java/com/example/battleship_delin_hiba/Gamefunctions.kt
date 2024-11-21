07fba221e3bbfe0c3d39e553f61851ed13b4dba1
hibaSabbagh <sahi22nn@student.ju.se>
2024-11-21 13:00:21 +0100

    finished game layout

--- app/src/main/java/com/example/battleship_delin_hiba/Gamefunctions.kt
+++ app/src/main/java/com/example/battleship_delin_hiba/Gamefunctions.kt
@@ -56,7 +56,7 @@ fun BattleScreen(navController: NavController){
     val boardData = Array(tiles) { Array(tiles) { 0 } }
     for(i in boardData.indices){
         for(j in boardData[i].indices){
-            boardData[i][j] = (0..1).random()
+            boardData[i][j] = 0    //(0..1).random()
         }
     }
 
@@ -116,12 +116,12 @@ fun BattleScreen(navController: NavController){
                                 color = if (boardData[row][column] == 0) {
                                     Color.White
                                 } else {
-                                    Color.Red
+                                    Color.Gray
                                 },
                                 shape = RectangleShape
                             )
                             .size(30.dp)
-                            .border(1.dp, Color.Black).clickable{}
+                            .border(1.dp, Color.Black).clickable{ boardData[row][column] = 1 }
 
 
                         )
@@ -150,7 +150,7 @@ fun BattleScreen(navController: NavController){
                                 shape = RectangleShape
                             )
                             .size(15.dp)
-                            .border(1.dp, Color.Black).clickable{}
+                            .border(1.dp, Color.Black)
                     )
                 }
             }
---
+++ app/src/main/java/com/example/battleship_delin_hiba/setUpBoardFunctions.kt
@@ -0,0 +1,95 @@
+package com.example.battleship_delin_hiba
+
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SetUpBoardScreen(navController: NavController, playerList: MutableList<Player>, battlesList: MutableList<Battle>)
+{
+    val tiles = 10
+    val boardDataChange = Array(tiles) { Array(tiles) { 0 } }
+    for(i in boardDataChange.indices){
+        for(j in boardDataChange[i].indices){
+            boardDataChange[i][j] = 0 //(0..1).random()
+        }
+    }
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Text(text = "Set Up Board")
+                    }
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.popBackStack()
+                        }
+                    ){
+                        Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "back"
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { navController.navigate("Battle") },
+                modifier = Modifier.padding(16.dp),
+                shape = CircleShape,
+                containerColor = Color(0xFFD3368E),
+                contentColor = Color.Black,
+                content = { Text("Start Game") }
+            )
+        },
+        content = { padding ->
+            Column (modifier = Modifier
+                .padding(padding)
+                .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ){
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(tiles),
+                    modifier = Modifier
+                        .padding(start = 50.dp, end = 50.dp, top = 50.dp).fillMaxWidth().height(400.dp)
+                ){
+                    itemsIndexed(boardDataChange.flatten()){ index, tileValue ->
+                        val row = index / tiles
+                        val column = index % tiles
+                        Box(Modifier.fillMaxSize()
+                                .background(
+                                    color = if (boardDataChange[row][column] == 0) {
+                                        Color.White
+                                    } else {
+                                        Color.LightGray
+                                    },
+                                    shape = RectangleShape
+                                )
+                                .size(30.dp)
+                                .border(1.dp, Color.Black).clickable { boardDataChange[row][column] = 1 }
+                        )
+                    }
+                }
+            }
+        }
+    )
+
+}
