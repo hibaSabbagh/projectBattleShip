4f6f2dabed85e7fa7b925ef4cdbfd9f1b1d225c5
hibaSabbagh <sahi22nn@student.ju.se>
2024-11-25 15:54:31 +0100

    test

--- app/src/main/java/com/example/battleship_delin_hiba/Gamefunctions.kt
+++ app/src/main/java/com/example/battleship_delin_hiba/Gamefunctions.kt
@@ -157,6 +157,8 @@ fun BattleScreen(navController: NavController, model: GameModel){
 fun handleLeaveGame(navController: NavController, model: GameModel){
     model.localBattleId.value?.let {
         model.db.collection("battles").document(it).delete().addOnSuccessListener {
+            model.localBattleId.value = null
+            model.localBoardId.value = null
             navController.navigate("Lobby") {
                 popUpTo("SetUpBoard") { inclusive = true }
             }
